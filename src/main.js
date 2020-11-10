72af12ff84478fe520543e37f69598657f8649ae
itzhanglv <itzhanglv@163.com>
2020-11-11 00:06:19 +0800

    v2

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import App from './App.vue'
 Vue.config.productionTip = true
 Vue.config.productionTip = true
 Vue.config.productionTip = true
+
 Vue.config.productionTip = true
 Vue.config.productionTip = true
 Vue.config.productionTip = true
+Vue.config.productionTip = false
+Vue.config.productionTip = false
+Vue.config.productionTip = false
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
